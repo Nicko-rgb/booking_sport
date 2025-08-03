6c8ff851e17f434ab3b77a0921f8fbdf1b610b2b
Nicko-rgb <mancillanixon7@gmail.com>
2025-08-03 12:57:13 -0500

    correccion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import  "../styles/header.css"
 import Header from "../components/layout/header/Header";
 {/* <styles /> */}
-const home = ({id=""}) => {
+const Home = ({id=""}) => {
   return (
     <div>
          {/* <!-- Hero Section --> */}
@@ -148,4 +148,4 @@ const home = ({id=""}) => {
   );
 };
 
-export default home;
+export default Home;
